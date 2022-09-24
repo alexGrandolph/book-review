9b305ad185d600606bb5bc5de1ec57f9b17271b4
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-24 16:32:16 -0600

    feat: add html elements and images for book image container

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,10 +1,26 @@
 import './Container.css'
+import { useRef } from "react"
+import { motion } from "framer-motion"
+
+import ThreeBody from '../../assets/images/3-body.png'
+import TheStand from '../../assets/images/the-stand.png'
+import Hyperion from '../../assets/images/hyperion.png'
 
 import React from 'react'
 
 const Container = () => {
   return (
-    <div>Container</div>
+    <div className="container-container">
+      <div className="droppable-area">
+       <img className="book-image" src={TheStand} alt="book-cover-image" />
+       <img className="book-image" src={ThreeBody} alt="book-cover-image" />
+       <img className="book-image" src={Hyperion} alt="book-cover-image" />
+      </div>
+
+
+
+
+    </div>
   )
 }
 
